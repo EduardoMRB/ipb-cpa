233d312d521589bc5f8a05770853dbdb3b8f509a
Eduardo Borges <eduardomrb@gmail.com>
2015-06-10 14:54:56 -0400

    Schedule view
    ipb-cpa.site.schedule builds the schedule view component from the
    database.

--- src/ipb_cpa/site/schedule.clj
+++ src/ipb_cpa/site/schedule.clj
@@ -1,14 +1,46 @@
 (ns ipb-cpa.site.schedule
-  (:require [ipb-cpa.db :as db]))
+  (:require [ipb-cpa.db :as db]
+            [clj-time.format :as f]))
 
-(defn- build-schedule-row [{:keys [day_of_the_week description time]}]
+(def ^:private days-of-the-week
+  ["Segunda" "Terça" "Quarta" "Quinta" "Sexta" "Sábado" "Domingo"])
+
+(defn- sort-events [events]
+  (sort (fn [a b]
+          (let [fmt (f/formatter "H:m'h")
+                a-date (f/parse fmt (:time a))
+                b-date (f/parse fmt (:time b))]
+            (compare a-date b-date)))
+        events))
+
+(defn- sort-days-of-week [schedule]
+  (sort (fn [a b]
+          (let [a-day (first a)
+                b-day (first b)]
+            (compare (.indexOf days-of-the-week a-day)
+                     (.indexOf days-of-the-week b-day))))
+        schedule))
+
+(defn- transform-schedules [schedules]
+  (let [xformed (reduce (fn [xfrmd schedule]
+                          (update-in xfrmd
+                                     [(:day_of_the_week schedule)]
+                                     #(if (seq %)
+                                        (sort-events (conj % schedule))
+                                        [schedule])))
+                        {}
+                        schedules)]
+    (sort-days-of-week xformed)))
+
+(defn- build-schedule-row [[day_of_the_week schedules]]
   (list
-    [:h3 day_of_the_week]
-    [:ul
-     [:li [:em time] (str " - " description)]]))
+   [:h3 day_of_the_week]
+   [:ul
+    (for [{:keys [time description]} schedules]
+         [:li [:em time] (str " - " description)])]))
 
 (defn get-schedule-view []
   (if-let [schedules (seq (db/get-schedules nil))]
     (mapcat
-      build-schedule-row
-      schedules)))
+     build-schedule-row
+     (transform-schedules schedules))))
--- test/ipb_cpa/site/schedule_test.clj
+++ test/ipb_cpa/site/schedule_test.clj
@@ -10,9 +10,10 @@
                                      :description "Desc"
                                      :time "19h"}])]
     (get-schedule-view)) => (list
-                              [:h3 "Segunda"]
-                              [:ul
-                               [:li [:em "19h"] " - Desc"]]))
+                             [:h3 "Segunda"]
+                             [:ul
+                              (list
+                               [:li [:em "19h"] " - Desc"])]))
 
 (fact "multiple schedules gets joined together"
   (with-redefs [db/get-schedules (fn [_]
@@ -25,13 +26,90 @@
                                      :description "Another Desc"
                                      :time "20:30h"}])]
     (get-schedule-view)) => (list
-                              [:h3 "Segunda"]
-                              [:ul
-                               [:li [:em "19h"] " - Desc"]]
-                              [:h3 "Quarta"]
-                              [:ul
-                               [:li [:em "20:30h"] " - Another Desc"]]))
+                             [:h3 "Segunda"]
+                             [:ul
+                              (list
+                               [:li [:em "19h"] " - Desc"])]
+                             [:h3 "Quarta"]
+                             [:ul
+                              (list
+                               [:li [:em "20:30h"] " - Another Desc"])]))
 
 (fact "returns nil if there is no schedule in database"
   (with-redefs [db/get-schedules (fn [_][])]
     (get-schedule-view)) => nil)
+
+(fact "repeated days of the week get stacked up"
+  (with-redefs [db/get-schedules (fn [_]
+                                   [{:id 1
+                                     :day_of_the_week "Segunda"
+                                     :description "Desc"
+                                     :time "19:00h"}
+                                    {:id 3
+                                     :day_of_the_week "Segunda"
+                                     :description "Other desc"
+                                     :time "20:30h"}
+                                    {:id 2
+                                     :day_of_the_week "Quarta"
+                                     :description "Another Desc"
+                                     :time "20:30h"}])]
+    (get-schedule-view)) => (list
+                             [:h3 "Segunda"]
+                             [:ul
+                              (list
+                               [:li [:em "19:00h"] " - Desc"]
+                               [:li [:em "20:30h"] " - Other desc"])]
+                             [:h3 "Quarta"]
+                             [:ul
+                              (list
+                               [:li [:em "20:30h"] " - Another Desc"])]))
+
+(fact "repeated schedules gets sorted by time"
+  (with-redefs [db/get-schedules (fn [_]
+                                   [{:id 1
+                                     :day_of_the_week "Segunda"
+                                     :description "Desc"
+                                     :time "22:00h"}
+                                    {:id 3
+                                     :day_of_the_week "Segunda"
+                                     :description "Other desc"
+                                     :time "20:30h"}
+                                    {:id 2
+                                     :day_of_the_week "Quarta"
+                                     :description "Another Desc"
+                                     :time "20:30h"}])]
+    (get-schedule-view)) => (list
+                             [:h3 "Segunda"]
+                             [:ul
+                              (list
+                               [:li [:em "20:30h"] " - Other desc"]
+                               [:li [:em "22:00h"] " - Desc"])]
+                             [:h3 "Quarta"]
+                             [:ul
+                              (list
+                               [:li [:em "20:30h"] " - Another Desc"])]))
+
+(fact "repeated schedules gets sorted by day of the week"
+  (with-redefs [db/get-schedules (fn [_]
+                                   [{:id 2
+                                     :day_of_the_week "Quarta"
+                                     :description "Another Desc"
+                                     :time "20:30h"}
+                                    {:id 1
+                                     :day_of_the_week "Segunda"
+                                     :description "Desc"
+                                     :time "22:00h"}
+                                    {:id 3
+                                     :day_of_the_week "Segunda"
+                                     :description "Other desc"
+                                     :time "20:30h"}])]
+    (get-schedule-view)) => (list
+                             [:h3 "Segunda"]
+                             [:ul
+                              (list
+                               [:li [:em "20:30h"] " - Other desc"]
+                               [:li [:em "22:00h"] " - Desc"])]
+                             [:h3 "Quarta"]
+                             [:ul
+                              (list
+                               [:li [:em "20:30h"] " - Another Desc"])]))
