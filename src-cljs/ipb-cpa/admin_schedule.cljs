100597d4abbb4b16315384b519da534457a09515
Eduardo Borges <eduardomrb@gmail.com>
2015-06-22 11:43:00 -0400

    Wire up tab clicks and starts schedule-list comp
    Tabs are getting activated correctly with use of core.async channels.
    
    The schedule-list component is started, next I'm going to seed the dev
    database to make it easier to see the component in actions in the
    browser.

--- src-cljs/ipb-cpa/admin_schedule.cljs
+++ src-cljs/ipb-cpa/admin_schedule.cljs
@@ -3,17 +3,20 @@
   (:require [om.core :as om]
             [om.dom :as dom]
             [ajax.core :refer [GET]]
-            [cljs.core.async :as async :refer [chan put! <! >!]]))
+            [cljs.core.async :as async :refer [chan put! <! >!]]
+            [clojure.string :as s]))
+
+(enable-console-print!)
 
 (def app-state (atom {:schedules []
-                      :active-tab "Seg"
-                      :days-of-the-week [{:day "Seg" :active? true}
-                                         {:day "Ter" :active? false}
-                                         {:day "Quar" :active? false}
-                                         {:day "Quin" :active? false}
-                                         {:day "Sex" :active? false}
-                                         {:day "Sab" :active? false}
-                                         {:day "Dom" :active? false}]}))
+                      :days-of-the-week (array-map
+                                         :seg true
+                                         :ter false
+                                         :quar false
+                                         :quin false
+                                         :sex false
+                                         :sab false
+                                         :dom false)}))
 
 ;; Ajax stuff
 (defn handler [resp]
@@ -26,21 +29,24 @@
      {:handler handler
       :error-handler err-handler})
 
+;; Util functions
+(defn tab-name [tab-keyword]
+  (s/capitalize (s/replace (str tab-keyword) ":" "")))
+
 ;; Om components
-(defn tab [{:keys [day active?]} owner]
+(defn tab [[day active?] owner]
   (reify
    om/IRenderState
    (render-state [_ {:keys [active]}]
      (dom/li #js {:className (str "tab-title" (if active? " active"))}
        (dom/a #js {:onClick #(put! active day)}
-              day)))))
+              (tab-name day))))))
 
 (defn tabs [data owner]
   (reify
    om/IInitState
    (init-state [_]
-     {:active (chan)
-      :active-tab "Seg"})
+     {:active (chan)})
    om/IWillMount
    (will-mount [_]
      (let [active (om/get-state owner :active)]
@@ -49,8 +55,12 @@
           (let [active-tab (<! active)]
             (om/transact! data
                           (fn [app-state]
-                            ;; TODO: Change app-state structure to a map
-                            app-state)))))))
+                            (let [deactivated-dow (apply array-map
+                                                         (flatten (map (fn [[day _]]
+                                                                         [day false])
+                                                                       (:days-of-the-week app-state))))
+                                  new-dow (assoc deactivated-dow active-tab true)]
+                              (assoc app-state :days-of-the-week new-dow)))))))))
    om/IRenderState
    (render-state [_ {:keys [active]}]
      (apply dom/ul #js {:className "tabs"}
@@ -58,15 +68,44 @@
                           (:days-of-the-week data)
                           {:init-state {:active active}})))))
 
+(defn schedule-line [schedule owner]
+  (reify
+   om/IRender
+   (render [_]
+     (dom/li nil (:days-of-the-week schedule)))))
+
+(defn schedule-list [data owner]
+  (reify
+   om/IRender
+   (render [_]
+     (let [mappings {:seg "Segunda" :ter "Terça" :quar "Quarta" :quin "Quinta"
+                     :sex "Sexta" :sab "Sábado" :dom "Domingo"}
+           active-tab (->> (:days-of-the-week data)
+                           (filter (fn [[_ active?]]
+                                     active?))
+                           (ffirst)
+                           (mappings))
+           schedule (->> (:schedules data)
+                         (filter (fn [[dow _]]
+                                   (= active-tab dow))))
+           sch (if (seq schedule)
+                 (-> schedule
+                     (first)
+                     (second))
+                 [])]
+       (apply dom/ul nil
+              (om/build-all schedule-line sch))))))
+
 (defn schedule [data owner]
   (reify
    om/IRender
    (render [_]
      (dom/div nil
        (dom/h2 nil "Schedule Component")
-       (om/build tabs data)))))
+       (om/build tabs data)
+       (om/build schedule-list data)))))
 
 (om/root
  schedule
  app-state
- {:target (.getElementById js/document "schedule-component")})
+ {:target (.querySelector js/document "#schedule-component")})
