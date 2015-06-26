5540a2322dd03f888fbcf3834bd1481f6687364f
Eduardo Borges <eduardomrb@gmail.com>
2015-06-26 12:09:26 -0400

    Fix schedule seeds
    Change the time 8:30 to 08:30.

--- migrations/20150622122400-schedule-seed.up.sql
+++ migrations/20150622122400-schedule-seed.up.sql
@@ -1,4 +1,4 @@
 INSERT INTO schedule (day_of_the_week, description, time) VALUES
-('Domingo', 'Escola biblica dominical', '8:30h'),
+('Domingo', 'Escola biblica dominical', '08:30h'),
 ('Domingo', 'Culto de adoracao', '19:00h'),
 ('Quinta', 'Reuniao de oracao', '19:30h');
