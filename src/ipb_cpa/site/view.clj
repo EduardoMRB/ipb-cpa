2cb80e6860a73a879f277831c523c5c9032f09aa
Eduardo Borges <eduardomrb@gmail.com>
2015-07-06 12:42:40 -0400

    Remove unecessary rows

--- src/ipb_cpa/site/view.clj
+++ src/ipb_cpa/site/view.clj
@@ -15,7 +15,7 @@
     [:li [:a {:href "#"} "Missões"]]]])
 
 (defn header []
-  [:div.row.site-header
+  [:div.site-header
    [:a.large-2.large-offset-0.small-offset-3.small-6.columns {:href (url-for :site#index)}
     [:img {:src "images/ipb-logo-without-text.png"}]]
    (menu)])
@@ -33,12 +33,13 @@
             ;; header.
             (header)
             ;; Site content.
-            [:main.row.site-content
+            [:main.site-content
              body]
             ;; Site footer.
             [:footer.site-footer
-             [:h2 "Como chegar"]
              ;; Google Maps iframe.
+             [:div.small-12.columns
+              [:h2 "Como chegar"]]
              [:div.small-12.columns.google-maps
               [:iframe {:src "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d480.43820863243894!2d-56.036553589325415!3d-15.564608827688991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x08cb3ad878331829!2s1%C2%B0+Igreja+Presbiteriana+do+CPA+4!5e0!3m2!1spt-BR!2sbr!4v1433469171863"
                         :width "1140"
@@ -51,7 +52,7 @@
            [:script "goog.require('ipb_cpa.core');"]]]]))
 
 (defn random-verses []
-  [:div.small-12.large-6.columns
+  [:div.small-12.large-6.small-collapse.columns
    [:h2 "Versículo do dia"]
    [:p (:text (get-verse))]
    [:em (:reference (get-verse))]])
@@ -62,7 +63,7 @@
    (schedule/get-schedule-view (db/get-schedules database))])
 
 (defn home-first-row [db]
-  [:div
+  [:div.columns
    (weekly-schedule db)
    (random-verses)])
 
