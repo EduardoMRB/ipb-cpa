72b615b613bc3dc3df1d1cfd6e25ca70c0a16b19
Eduardo Borges <eduardomrb@gmail.com>
2015-07-07 12:33:39 -0400

    Add TestDatabase component in service-test
    Added but the tests aren't running at all and I can't find out why. The
    tests begin to fail if I attach the `test-system-interceptor` into the
    `service-map` when creating a servlet.
    
    Investigating on some issues on pedestal gh page I cought an idea to
    create a server instead of a servlet for this tests.

--- test/ipb_cpa/service_test.clj
+++ test/ipb_cpa/service_test.clj
@@ -6,20 +6,30 @@
             [com.stuartsierra.component :as component]
             [environ.core :refer [env]]
             [ipb-cpa.service :as service]
-            [ipb-cpa.system :as system]
             [ipb-cpa.server :as server]))
 
 ;; Test system.
+(defrecord TestDatabase [subprotocol subname]
+  component/Lifecycle
+  (start [component]
+    (assoc component :db {:subprotocol subprotocol
+                          :subname subname}))
+  (stop [component]
+    (dissoc component :db)))
+
 (def test-system
+  (component/system-map
+   :database (->TestDatabase "sqlite" ":memory:")))
+
+(def test-system-interceptor
   (interceptor/on-request
    (fn [request]
-     (let [system-map (system/system (env :test-db-connection-uri))]
-       (assoc request :system (component/start system-map))))))
+     (assoc request :system (component/start test-system)))))
 
 (def service
-  (::bootstrap/service-fn (bootstrap/create-servlet
-                           (server/attach-interceptor service/service
-                                                      test-system))))
+  (::bootstrap/service-fn (-> service/service
+                              (server/attach-interceptor test-system-interceptor)
+                              bootstrap/create-servlet)))
 
 ;; Tests.
 (facts home-page-test
