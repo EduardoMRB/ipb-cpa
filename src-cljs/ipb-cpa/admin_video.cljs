f2ef9461ed834d7474e0862ac972a0c8434011c4
Eduardo Borges <eduardomrb@gmail.com>
2015-07-08 11:41:19 -0400

    Initial markup for new-video component
    Build the markup of the form, no behavior implemented yet.

--- src-cljs/ipb-cpa/admin_video.cljs
+++ src-cljs/ipb-cpa/admin_video.cljs
@@ -9,12 +9,62 @@
 
 (def app-state (atom {}))
 
+(defn new-video [data owner]
+  (reify
+   om/IRender
+   (render [_]
+     (html
+       [:form
+        [:fieldset
+         [:legend "Adicionar video"]
+         [:div.row
+          [:div.small-6.columns
+           [:label "Título"
+            [:input {:type "text"
+                     :on-change #(.log js/console "changed title")
+                     :ref "title"}]]]
+          [:div.small-6.columns
+           [:label "Data"
+            [:input {:type "text"
+                     :on-change #(.log js/console "changed date")
+                     :ref "date"}]]]]
+         [:div.row
+          [:div.small-12.columns
+           [:label "Resumo"
+            [:textarea {:on-change #(.log js/console "changed excerpt")
+                        :ref "exerpt"
+                        :rows 10}]]]]
+         [:div.row.collapse
+          [:div.small-6.columns
+           [:div.small-12.columns
+            [:label "Incorporar"
+             [:input {:type "text"
+                      :on-change #(.log js/console "embedded changed")
+                      :ref "embedded"}]]]
+           [:div.small-12.columns
+            [:label "Ativo?"]
+            [:div.switch
+             [:input#active {:type "checkbox"
+                             :on-change #(.log js/console "active changed")
+                             :ref "active"}]
+             [:label {:for "active"}]]]]
+          [:div.small-6.columns
+           [:div.flex-video]]]
+         [:div.row
+          [:button.small.right {:type "button"
+                    :on-click #(.log js/console "new-button clicked")
+                    :ref "new-button"}
+           "Criar"]]]]))))
+
 (defn video [data owner]
   (reify
    om/IRender
    (render [_]
      (html
-       [:h2 "Video component"]))))
+       [:div
+        [:h2 "Video component"]
+        [:div
+         (om/build new-video data)]]))))
 
 (om/root video
          app-state
--- src/ipb_cpa/view/admin_view.clj
+++ src/ipb_cpa/view/admin_view.clj
@@ -14,12 +14,12 @@
             [:link {:href "/css/app.css"
                     :rel "stylesheet"}]
             [:body
-             [:div.row
+             [:div
               ;; Content.
-              [:main.row.admin-content
+              [:main.admin-content
                body]
               ;; Footer.
-              [:footer.row.admin-footer]
+              [:footer.admin-footer]
               ;; JavaScript resources.
               [:script {:src "/js/out/goog/base.js"}]
               [:script {:src "/js/app.js"}]
@@ -32,5 +32,5 @@
 
 (defn video-index [db]
   (layout {:body (list [:h1 "Videos - Admin"]
-                       [:div#video-component.row])
+                       [:div#video-component])
            :scripts [:script "goog.require('ipb_cpa.admin_video');"]}))
